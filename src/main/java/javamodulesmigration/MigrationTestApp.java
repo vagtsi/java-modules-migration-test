bb4f09d6ebc18d05cccda0e749b84f32de1879b8
Jens Vagts <jens.vagts@automationhero.ai>
2022-02-04 13:41:58 +0100

    added logging output only

--- src/main/java/javamodulesmigration/MigrationTestApp.java
+++ src/main/java/javamodulesmigration/MigrationTestApp.java
@@ -1,11 +1,15 @@
 package javamodulesmigration;
 
-import static org.elasticsearch.node.NodeBuilder.*;
 import org.elasticsearch.node.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MigrationTestApp {
+  private static final Logger log = LoggerFactory.getLogger(MigrationTestApp.class);
 
   public static void main(String[] args) {
+    log.info("Starting elastic search node");
+
     Node server = nodeBuilder().build();
     server.start();
   }
