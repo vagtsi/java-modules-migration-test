7367aa6bcb68329ae84e6586c4f900cc4e041e48
Jens Vagts <jens.vagts@automationhero.ai>
2022-02-10 11:41:11 +0100

    added java.annotation dependency

--- immutables/src/main/java/module-info.java
+++ immutables/src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module javamodulemigration.immutables {
+  // logging
   requires org.slf4j;
   
-  // immutables
+  // immutables with dependencies
   requires static java.compiler;
   requires static org.immutables.value;
-//  requires org.immutables.criteria.inmemory;
   requires org.immutables.mongo;
   requires org.immutables.gson;
   requires com.google.gson;
-
+  requires java.annotation;
+  requires java.validation;
 }
