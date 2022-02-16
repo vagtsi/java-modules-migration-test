6202fb51ff05c94746a57656209d726560d2625b
Jens Vagts <jens.vagts@automationhero.ai>
2022-02-16 17:07:27 +0100

    added comment only

--- elasticsearch-app/src/main/java/module-info.java
+++ elasticsearch-app/src/main/java/module-info.java
@@ -5,5 +5,7 @@ module javamodulemigration.elasticsearch.app {
   requires org.apache.httpcomponents.httpcore;
   requires org.apache.httpcomponents.httpclient;
   requires org.apache.httpcomponents.httpasyncclient;
+  
+  //jackson parser requires reflection access
   opens javamodulesmigration.elasticsearch;
 }
